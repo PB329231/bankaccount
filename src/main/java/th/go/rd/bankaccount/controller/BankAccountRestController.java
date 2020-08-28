505c4a5f6e256a621ff3a2f68623ba387b0943f5
PB329231 <Administrator@1G93TF2.rd.go.th>
2020-08-28 12:00:43 +0700

    API and Web Service

--- src/main/java/th/go/rd/bankaccount/controller/BankAccountRestController.java
+++ src/main/java/th/go/rd/bankaccount/controller/BankAccountRestController.java
@@ -1,9 +1,6 @@
 package th.go.rd.bankaccount.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import th.go.rd.bankaccount.data.BankAccountRepository;
 import th.go.rd.bankaccount.model.BankAccount;
 
@@ -19,6 +16,11 @@ public class BankAccountRestController {
         this.repository = repository;
     }
 
+    @GetMapping("/customer/{id}")
+    public List<BankAccount> getAllCustomerId(@PathVariable int id) {
+        return repository.findByCustomerId(id);
+    }
+
     @GetMapping
     public List<BankAccount> getAll(){
         return repository.findAll();
@@ -27,6 +29,29 @@ public class BankAccountRestController {
     @GetMapping("/{id}")
     public BankAccount getOne(@PathVariable int id){
         return repository.findById(id).get();
+    }
+
+    @PostMapping
+    public BankAccount create(@RequestBody BankAccount bankAccount){ //@RequestBody ส่งเป็น JSON /==>รับ JSON มาจาก user
+        repository.save(bankAccount); // method save จะส่งค่าไปเก็บ
+        return repository.findById(bankAccount.getId()).get(); //คืนค่าเป็นobj หรือคืนค่า bankaccount ไปเลยก็ได้
+    }
+
+
+    @PutMapping("/{id}")
+    public BankAccount update(@PathVariable int id,
+                              @RequestBody BankAccount bankAccount) {
+        BankAccount record = repository.findById(id).get(); //ดึงข้อมูลใน account ด้วย findById  มาเก็บใน record
+        record.setBalance(bankAccount.getBalance());
+        repository.save(record);//save ค่ากลับเข้าไป
+        return record;
+    }
+
+    @DeleteMapping("/{id}")
+    public BankAccount delete(@PathVariable int id) {
+        BankAccount record = repository.findById(id).get();
+        repository.deleteById(id);
+        return record;
     }
 
 }
